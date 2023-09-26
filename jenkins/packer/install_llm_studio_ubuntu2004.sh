5def9cf39732cae76645b1661d424f9593ca85a9
DinukaH2O <dinuka.appuhamy@h2o.ai>
2023-09-26 09:56:49 +0530

    ci:fix2

--- jenkins/packer/install_llm_studio_ubuntu2004.sh
+++ jenkins/packer/install_llm_studio_ubuntu2004.sh
@@ -104,7 +104,7 @@ server {
 printf """
 ip=\$(dig +short myip.opendns.com @resolver1.opendns.com)
 sed \"s/<|_SUBST_PUBLIC_IP|>;/\$ip;/g\" /home/ubuntu/temp.conf  > /etc/nginx/conf.d/llm.conf
-sudo systemctl enable nginx
+sudo systemctl restart nginx
 """ > run_nginx.sh
 
 sudo chmod u+x run_nginx.sh
@@ -126,8 +126,7 @@ WantedBy=multi-user.target
 """ > llm_nginx.service
 
 sudo systemctl daemon-reload
-sudo systemctl restart nginx
-sudo systemctl restart llm_studio.service
+sudo systemctl enable llm_studio.service
 
 
 
